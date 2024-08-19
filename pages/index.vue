db11f21b820b66b88ef47cccfda746f90595c55e
Zikam1 <isaac.aku@cyphercrescent.com>
2024-08-19 06:50:06 +0100

    feat:completed workshop

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="text-center py-20 bg-gray-100">
-    <h2 class="text-4xl font-bold mb-8">NWAICHI, Eucharia Oluchi </h2>
+    <h2 class="text-4xl font-bold mb-8">PROF. NWAICHI, Eucharia Oluchi </h2>
     <p class="text-xl mb-8">Explore the research, publications, and more.</p>
     <NuxtLink to="/publications" class="bg-blue-900 text-white py-2 px-4 rounded hover:bg-blue-800">Learn More</NuxtLink>
   </section>
